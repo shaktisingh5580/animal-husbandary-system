7d6c671d96703f2173532a8c2db4f36f8eddfafe
SHAKTI SINGH <shaktisingh7227077@gmail.com>
2025-03-23 16:36:35 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 // Supabase Configuration
 const SUPABASE_CONFIG = {
-  url: "https://kstfouovkbnadirodpio.supabase.co",
+  url: "https://talqdxageqbvsavscyqt.supabase.co",
   anonKey:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzdGZvdW92a2JuYWRpcm9kcGlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxMTU3MDUsImV4cCI6MjA1NzY5MTcwNX0._ywN27wvbd0NB35hGdANnmgtJ9II1YBy2MRyco5dm9g",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbHFkeGFnZXFidnNhdnNjeXF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3Mjc0OTAsImV4cCI6MjA1ODMwMzQ5MH0.YBMSv0YClLrGSM16M5-GXNefh-5MbPQwjh-CBET0shU",
   serviceRoleKey:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzdGZvdW92a2JuYWRpcm9kcGlvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjExNTcwNSwiZXhwIjoyMDU3NjkxNzA1fQ.za0xWl7pcEijfYZ7r2VowLxUerHW84sOtQCIHM_8x_4",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbHFkeGFnZXFidnNhdnNjeXF0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjcyNzQ5MCwiZXhwIjoyMDU4MzAzNDkwfQ.O9eyG9xhAfD0AQARv-reR2CnTvf2TSsyRGxvbirlpko",
 }
 
